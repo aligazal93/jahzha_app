58206555642efb6e69fc478e99dac2c393154337
Ali <aligazal.wd93@gmail.com>
2024-12-30 19:55:11 +0200

    - User Icon in MenuBar

--- lib/views/menu/units/info_card.dart
+++ lib/views/menu/units/info_card.dart
@@ -1,56 +1,88 @@
 part of '../view.dart';
+
 class InfoCard extends StatelessWidget {
   const InfoCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    String initials = CachingUtils.user?.data.name?.isNotEmpty ?? false ? CachingUtils.user!.data.name!.split(' ').map((word) => word[0]).take(2).join() : "";
     return Container(
       decoration: BoxDecoration(
-          border: Border.all(
-              width: 2,
-              color: AppColors.tGray
-          ),
-          borderRadius: BorderRadius.circular(12)
-      ),
-      padding: EdgeInsets.symmetric(vertical: 8,horizontal: 12),
-      margin: EdgeInsets.symmetric(vertical: 20,horizontal: 12),
+          border: Border.all(width: 2, color: AppColors.tGray),
+          borderRadius: BorderRadius.circular(12)),
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+      margin: EdgeInsets.symmetric(vertical: 20, horizontal: 12),
       child: Row(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(50),
-            child: Image.asset(
-              'assets/images/ma.png',
-              fit: BoxFit.fill,
-              width: 50,
-              height: 50,
-              errorBuilder: (context, error, stackTrace) => Container(
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                    color: AppColors.primary
+          // ClipRRect(
+          //   borderRadius: BorderRadius.circular(50),
+          //   child: Image.asset(
+          //     'assets/images/ma.png',
+          //     fit: BoxFit.fill,
+          //     width: 50,
+          //     height: 50,
+          //     errorBuilder: (context, error, stackTrace) => Container(
+          //       width: 50,
+          //       height: 50,
+          //       decoration: BoxDecoration(
+          //           color: AppColors.primary
+          //       ),
+          //       child: Icon(
+          //         FontAwesomeIcons.image,
+          //         size: 20,
+          //         color: AppColors.white,
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          CachingUtils.user?.data.name == null
+              ? Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: AppColors.GrayLight,
+                      borderRadius: BorderRadius.circular(150)),
+                  child: Center(
+                    child: Image.asset(
+                      'assets/images/logo.png',
+                      width: 20,
+                    ),
+                  ),
+                )
+              : Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: AppColors.GrayLight,
+                      borderRadius: BorderRadius.circular(150)),
+                  child: Center(
+                    child: AppText(
+                      title: initials,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                      color: AppColors.primary,
+                    ),
+                  ),
                 ),
-                child: Icon(
-                  FontAwesomeIcons.image,
-                  size: 20,
-                  color: AppColors.white,
-                ),
-              ),
-            ),
-          ),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              CachingUtils.user?.data.name == null ? SizedBox() :
-              AppText(
-                title: CachingUtils.user!.data.name!,
-                padding: EdgeInsets.symmetric(horizontal: 8),
-                color: AppColors.secondary,
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
+              CachingUtils.user?.data.name == null
+                  ? SizedBox()
+                  : AppText(
+                      title: CachingUtils.user!.data.name!,
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      color: AppColors.secondary,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+              SizedBox(
+                height: 4,
               ),
-              SizedBox(height: 4,),
               AppText(
-                title: CachingUtils.user!.data.phoneNumber == null ? CachingUtils.user!.data.email! : CachingUtils.user!.data.phoneNumber!,
+                title: CachingUtils.user!.data.phoneNumber == null
+                    ? CachingUtils.user!.data.email!
+                    : CachingUtils.user!.data.phoneNumber!,
                 padding: EdgeInsets.symmetric(horizontal: 8),
                 color: AppColors.txtGray,
                 fontSize: 14,
@@ -62,13 +94,15 @@ class InfoCard extends StatelessWidget {
           AppButton(
             title: 'Edit'.tr(),
             titleFontSize: 12,
-            padding: EdgeInsets.symmetric(vertical: 0,horizontal: 30),
+            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 30),
             onTap: () {
               RouteUtils.navigateTo(AccountDetailsView());
             },
             height: 40,
           ),
-          SizedBox(width: 12,),
+          SizedBox(
+            width: 12,
+          ),
         ],
       ),
     );
