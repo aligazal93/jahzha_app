dd7695c4c6f1a3e2110989d1ebed9a4a6c821a2c
Jemi <ahmed.abogamil@sarmad.sa>
2025-01-04 13:44:12 +0200

    fix nav bar

--- lib/views/navbar/units/bottom_navbar.dart
+++ lib/views/navbar/units/bottom_navbar.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:jahzha_app/core/helpers/app_colors.dart';
+import 'package:jahzha_app/core/helpers/dimensions.dart';
+import 'package:jahzha_app/core/helpers/utils.dart';
 import 'package:jahzha_app/core/route_utils/route_utils.dart';
 import 'package:jahzha_app/views/send_shipments/view.dart';
 import 'package:jahzha_app/widgets/app_text.dart';
@@ -23,49 +25,52 @@ class _BottomNavBarState extends State<BottomNavBar> {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 110,
-      child: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-          Container(
-            height:80,
-            decoration: BoxDecoration(
-              color: AppColors.white.theme,
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(0, 0),
-                  blurRadius: 7,
-                  spreadRadius: .6,
-                  color: AppColors.tGray.theme,
-                ),
-              ],
-            ),
-            child: Row(
-              children: [
-                _bottomNavItem(
+      child: SafeArea(
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Container(
+              height: 80,
+              decoration: BoxDecoration(
+                color: AppColors.white.theme,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 0),
+                    blurRadius: 7,
+                    spreadRadius: .6,
+                    color: AppColors.tGray.theme,
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  _bottomNavItem(
                     index: 0,
                     icon: 'icon-1',
-                    title: 'Home'.tr()
-                ),
-                _bottomNavItem(
+                    title: 'Home'.tr(),
+                  ),
+                  _bottomNavItem(
                     index: 1,
                     icon: 'icon-2',
-                    title: 'My shipments'.tr()
-                ),
-                Spacer(),
-                _bottomNavItem(
-                  index: 2,
-                  title: 'Coupons'.tr(),
-                  icon: 'icon-4',
-                ),
-                _bottomNavItem(
-                  index: 3,
-                  title: 'Menu'.tr(),
-                  icon: 'icon-5',
-                ),
-              ],
+                    title: 'My shipments'.tr(),
+                  ),
+                  if (Utils.isAR) SizedBox(width: 20.width),
+                  Spacer(),
+                  if (Utils.isAR) SizedBox(width: 20.width),
+                  _bottomNavItem(
+                    index: 2,
+                    title: 'Coupons'.tr(),
+                    icon: 'icon-4',
+                  ),
+                  _bottomNavItem(
+                    index: 3,
+                    title: 'Menu'.tr(),
+                    icon: 'icon-5',
+                  ),
+                ],
+              ),
             ),
-          ),
-          Positioned(
+            Positioned(
               bottom: 10,
               left: 0,
               right: 0,
@@ -78,50 +83,66 @@ class _BottomNavBarState extends State<BottomNavBar> {
                     children: [
                       Container(
                         margin: EdgeInsets.symmetric(vertical: 14),
-                          width: 70,
-                          height: 70,
-                          decoration: BoxDecoration(
-                            color: AppColors.primary.theme,
-                            borderRadius: BorderRadius.circular(500),
-                          ),
-                          child: Image.asset('assets/images/icon-3.png'),),
+                        width: 70,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          color: AppColors.primary.theme,
+                          borderRadius: BorderRadius.circular(500),
+                        ),
+                        child: Image.asset('assets/images/icon-3.png'),
+                      ),
                       AppText(
                         title: 'Send Shipment'.tr(),
-                       fontSize: 12,
+                        fontSize: 12,
                         color: AppColors.primary,
                         fontWeight: FontWeight.w700,
                       )
                     ],
                   ),
                 ),
-              ))
-        ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
   Widget _bottomNavItem(
-      {required int index,required String title ,required String icon}) {
+      {required int index, required String title, required String icon}) {
     final cubit = NavBarCubit.of(context);
     final isActive = index == cubit.currentIndex;
-    return InkWell(
-      onTap: () => cubit.toggleTab(index),
-      child: Container(
-        decoration: BoxDecoration(
-          color: AppColors.white.theme,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 18,vertical: 10),
-              child: Image.asset('assets/images/$icon.png',
-                color: isActive ? AppColors.secondary.theme : AppColors.GrayLight.theme,
+    return Expanded(
+      child: InkWell(
+        onTap: () => cubit.toggleTab(index),
+        child: Container(
+          decoration: BoxDecoration(
+            color: AppColors.white.theme,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+                child: Image.asset(
+                  'assets/images/$icon.png',
+                  color: isActive
+                      ? AppColors.secondary.theme
+                      : AppColors.GrayLight.theme,
+                ),
               ),
-            ),
-            AppText(title: title, color:isActive ? AppColors.secondary.theme : AppColors.GrayLight.theme,fontWeight: FontWeight.w500, fontSize: 12,)
-          ],
+              AppText(
+                title: title,
+                color: isActive
+                    ? AppColors.secondary.theme
+                    : AppColors.GrayLight.theme,
+                fontWeight: FontWeight.w500,
+                fontSize: 12,
+              )
+            ],
+          ),
         ),
       ),
     );
