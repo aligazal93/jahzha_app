861118ab3d5067baa486eac7aaeb812dad0cbaa8
Jemi <ahmed.abogamil@sarmad.sa>
2025-01-28 15:13:31 +0200

    edit local notification

--- lib/core/firebase_messaging_utils/local_notification.dart
+++ lib/core/firebase_messaging_utils/local_notification.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:jahzha_app/core/helpers/dimensions.dart';
 
 import '../../widgets/app_text.dart';
@@ -13,40 +14,6 @@ import '../route_utils/route_utils.dart';
 
 class LocalNotification {
   static final instance = LocalNotification();
-  //
-  // final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
-  //     FlutterLocalNotificationsPlugin();
-  //
-  // Future<void> init() async {
-  //   const AndroidInitializationSettings initializationSettingsAndroid =
-  //       AndroidInitializationSettings('notification_icon');
-  //   final DarwinInitializationSettings initializationSettingsDarwin =
-  //       DarwinInitializationSettings(
-  //     requestAlertPermission: true,
-  //     requestBadgePermission: true,
-  //     requestSoundPermission: true,
-  //   );
-  //   final InitializationSettings initializationSettings =
-  //       InitializationSettings(
-  //     android: initializationSettingsAndroid,
-  //     iOS: initializationSettingsDarwin,
-  //   );
-  //   await _flutterLocalNotificationsPlugin.initialize(
-  //     initializationSettings,
-  //     onDidReceiveNotificationResponse: _onDidReceiveNotificationResponse,
-  //   );
-  //   if (Platform.isAndroid) {
-  //     await _flutterLocalNotificationsPlugin
-  //         .resolvePlatformSpecificImplementation<
-  //             AndroidFlutterLocalNotificationsPlugin>()
-  //         ?.requestNotificationsPermission();
-  //   }
-  // }
-  //
-  // void _onDidReceiveNotificationResponse(
-  //     NotificationResponse notificationResponse) {
-  //   RouteUtils.navigateTo(NotificationsView());
-  // }
 
   void show({
     required String title,
@@ -62,33 +29,7 @@ class LocalNotification {
         // RouteUtils.navigateTo(NotificationsView());
       },
     ).show();
-    // final id = DateTime.now().millisecondsSinceEpoch ~/ 1000;
-    // _flutterLocalNotificationsPlugin.show(
-    //   id,
-    //   title,
-    //   body,
-    //   NotificationDetails(
-    //     android: AndroidNotificationDetails(
-    //       'com.asparagus.user.app',
-    //       'Main',
-    //       // category: AndroidNotificationCategory.message,
-    //       importance: Importance.max,
-    //       playSound: true,
-    //       priority: Priority.high,
-    //     ),
-    //     iOS: DarwinNotificationDetails(
-    //       interruptionLevel: InterruptionLevel.timeSensitive,
-    //       presentSound: true,
-    //       presentList: true,
-    //       presentAlert: true,
-    //       presentBadge: true,
-    //       presentBanner: true,
-    //     ),
-    //   ),
-    // );
   }
-
-  bool _isVisible = false;
 }
 
 class _Notification extends StatefulWidget {
@@ -103,17 +44,24 @@ class _Notification extends StatefulWidget {
   final String body;
   final VoidCallback onTap;
 
-  Future<void> show() {
-    if (LocalNotification.instance._isVisible) {
-      RouteUtils.pop();
-      LocalNotification.instance._isVisible = false;
-    }
-    LocalNotification.instance._isVisible = true;
-    return showDialog(
-      barrierColor: Colors.transparent,
-      barrierDismissible: false,
-      context: RouteUtils.context,
-      builder: (context) => this,
+  Future<void> show() async {
+    final context = RouteUtils.context;
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: this,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        behavior: SnackBarBehavior.floating,
+        dismissDirection: DismissDirection.down,
+        padding: EdgeInsets.zero,
+        margin: EdgeInsets.fromLTRB(
+          8,
+          0,
+          8,
+          0,
+        ),
+      ),
     );
   }
 
@@ -122,98 +70,60 @@ class _Notification extends StatefulWidget {
 }
 
 class _NotificationState extends State<_Notification> {
-  Timer? timer;
-
-  @override
-  void initState() {
-    timer = Timer(
-      Duration(milliseconds: 4000),
-      dismiss,
-    );
-    super.initState();
-  }
-
-  void dismiss() {
-    if (mounted) {
-      RouteUtils.pop();
-    }
-  }
-
-  @override
-  void dispose() {
-    timer?.cancel();
-    LocalNotification.instance._isVisible = false;
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: RouteUtils.pop,
-      child: Material(
-        color: Colors.transparent,
-        child: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 16.width,
-              vertical: 16.height,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                FadeInDown(
-                  duration: Duration(milliseconds: 650),
-                  child: InkWell(
-                    onTap: () {
-                      dismiss();
-                      widget.onTap();
-                    },
-                    child: BasicCard(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 16.height,
-                        horizontal: 16.width
-                      ),
-                      child: Row(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(8),
-                            child: Image.asset(
-                              Utils.getAssetPNGPath('logo'),
-                              width: 40.width,
-                              height: 40.width,
-                            ),
-                          ),
-                          SizedBox(width: 16.width),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                AppText(
-                                  title: widget.title,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                                SizedBox(height: 4.height),
-                                AppText(
-                                  title: widget.body,
-                                  fontSize: 12.font,
-                                  fontWeight: FontWeight.w500,
-                                  color: AppColors.darkGrayBlue,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                      enableShadow: true,
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        FadeInUp(
+          duration: Duration(milliseconds: 650),
+          child: InkWell(
+            onTap: () {
+              // dismiss();
+              ScaffoldMessenger.of(RouteUtils.context).clearSnackBars();
+              widget.onTap();
+            },
+            child: BasicCard(
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset(
+                      Utils.getAssetPNGPath('logo-w'),
+                      width: 40.width,
+                      height: 40.width,
                     ),
                   ),
-                ),
-              ],
+                  SizedBox(width: 16.width),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AppText(
+                          title: widget.title,
+                          fontWeight: FontWeight.w600,
+                          color: AppColors.white,
+                        ),
+                        SizedBox(height: 4.height),
+                        AppText(
+                          title: widget.body,
+                          fontSize: 12.font,
+                          fontWeight: FontWeight.w500,
+                          color: AppColors.darkGrayBlue,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              enableShadow: true,
+              color: AppColors.secondary,
             ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
